35008f96df7f74844086267f509b61ca380d3dc4
EH-llOrJ <jth4115@naver.com>
2022-09-13 17:51:15 +0900

    테스트

---
+++ test2.js
@@ -0,0 +1,15 @@
+function isPrime(n) {
+  let arr = [];
+  for (let i = 2; i <= n; i++) {
+    if (n % i == 0) {
+      if (arr.indexOf(i) == -1) {
+        arr.push(i);
+      }
+      n = n / i;
+      i = 1;
+    }
+  }
+  console.log(arr);
+}
+
+isPrime(144);
